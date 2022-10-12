baf1839d96da52fb912e14ab623292f828fd667e
Fernando Augusto de Marins <f.augustomarins@gmail.com>
2022-10-11 21:16:20 -0300

    Converted the login view controller to view code.

--- On The Map/Controllers/LoginViewController.swift
+++ On The Map/Controllers/LoginViewController.swift
@@ -6,55 +6,177 @@
 //
 
 import UIKit
+import SnapKit
 
 class LoginViewController: UIViewController, UITextFieldDelegate {
     
     // MARK: - Variables
     
-    @IBOutlet weak var usernameTextField: UITextField!
-    @IBOutlet weak var passwordTextField: UITextField!
-    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
-    @IBOutlet weak var createAccountLabel: UILabel!
+    lazy var contentView: UIView! = {
+        let view = UIView()
+        view.backgroundColor = .white
+        return view
+    }()
+    
+    lazy var udacityLabel: UILabel = {
+        let label = UILabel()
+        label.text = "UDACITY"
+        label.font = UIFont.systemFont(ofSize: 29)
+        return label
+    }()
+    
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "On The Map"
+        label.font = UIFont.systemFont(ofSize: 25)
+        return label
+    }()
+    
+    lazy var usernameTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "E-mail"
+        textField.borderStyle = .roundedRect
+        textField.autocapitalizationType = .none
+        textField.delegate = self
+        return textField
+    }()
+    
+    lazy var passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Password"
+        textField.borderStyle = .roundedRect
+        textField.isSecureTextEntry = true
+        textField.delegate = self
+        return textField
+    }()
+    
+    lazy var loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.layer.cornerRadius = 15
+        button.backgroundColor = .blue
+        button.setTitle("Login", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.addTarget(self, action: #selector(login), for: .touchUpInside)
+        return button
+    }()
+    
+    lazy var activityIndicator: UIActivityIndicatorView = {
+        let activityIndicator = UIActivityIndicatorView()
+        activityIndicator.hidesWhenStopped = true
+        return activityIndicator
+    }()
+    
+    lazy var createAccountLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Don't have an account? Create one here"
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        let tap = UITapGestureRecognizer(target: self, action: #selector(openURL))
+        label.addGestureRecognizer(tap)
+        label.isUserInteractionEnabled = true
+        return label
+    }()
     
     // MARK: Lifecycle methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        activityIndicator.hidesWhenStopped = true
-        usernameTextField.delegate = self
-        passwordTextField.delegate = self
+        addViews()
+        addConstraints()
+    }
+    
+    private func addViews() {
+        view.addSubview(contentView)
+        contentView.addSubview(udacityLabel)
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(usernameTextField)
+        contentView.addSubview(passwordTextField)
+        contentView.addSubview(loginButton)
+        contentView.addSubview(activityIndicator)
+        contentView.addSubview(createAccountLabel)
+    }
+    
+    private func addConstraints() {
+        contentView.snp.makeConstraints {
+            $0.edges.equalTo(view.safeAreaLayoutGuide)
+        }
         
-        let tap = UITapGestureRecognizer(target: self, action: #selector(openURL))
-        createAccountLabel.addGestureRecognizer(tap)
-        createAccountLabel.isUserInteractionEnabled = true
+        udacityLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(32)
+            $0.height.equalTo(35)
+            $0.centerX.equalToSuperview()
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(udacityLabel.snp.bottom).offset(32)
+            $0.height.equalTo(35)
+            $0.centerX.equalToSuperview()
+        }
+        
+        usernameTextField.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(64)
+            $0.height.equalTo(35)
+            $0.leading.equalTo(contentView.snp.leading).offset(16)
+            $0.trailing.equalTo(contentView.snp.trailing).offset(-16)
+        }
+        
+        passwordTextField.snp.makeConstraints {
+            $0.top.equalTo(usernameTextField.snp.bottom).offset(12)
+            $0.height.equalTo(35)
+            $0.leading.equalTo(contentView.snp.leading).offset(16)
+            $0.trailing.equalTo(contentView.snp.trailing).offset(-16)
+        }
+        
+        activityIndicator.snp.makeConstraints {
+            $0.top.equalTo(passwordTextField.snp.bottom).offset(8)
+            $0.centerX.equalToSuperview()
+        }
+        
+        loginButton.snp.makeConstraints {
+            $0.top.equalTo(passwordTextField.snp.bottom).offset(64)
+            $0.height.equalTo(35)
+            $0.width.equalTo(220)
+            $0.centerX.equalToSuperview()
+        }
+        
+        createAccountLabel.snp.makeConstraints {
+            $0.bottom.equalToSuperview().offset(-24)
+            $0.height.equalTo(35)
+            $0.leading.equalTo(contentView.snp.leading).offset(16)
+            $0.trailing.equalTo(contentView.snp.trailing).offset(-16)
+        }
     }
     
     // MARK: - Login
     
-    @IBAction func login(_ sender: UIButton) {
+    @objc private func login() {
         showHideActivityIndicator(show: true, activityIndicator: activityIndicator)
-        print(usernameTextField.text!)
-        Client.login(username: usernameTextField.text!, password: passwordTextField.text!, completion: handleSessionResponse(success:error:))
+        Client.login(username: usernameTextField.text ?? "", password: passwordTextField.text ?? "", completion: handleSessionResponse(success:error:))
     }
     
-    
-    func handleSessionResponse(success: Bool, error: Error?) {
+    private func handleSessionResponse(success: Bool, error: Error?) {
         if success {
-            Client.getUserInfo { success, error in
+            Client.getUserInfo { [weak self] success, error in
+                guard let self = self else { return }
                 if success {
-                    self.showHideActivityIndicator(show: false, activityIndicator: self.activityIndicator)
+                    self.showHideActivityIndicator(show: false,
+                                                   activityIndicator: self.activityIndicator)
                     self.performSegue(withIdentifier: "login", sender: nil)
                 } else {
                     if let error = error {
-                        self.showHideActivityIndicator(show: false, activityIndicator: self.activityIndicator)
-                        self.showAlert(title: "Error", message: error.localizedDescription)
+                        self.showHideActivityIndicator(show: false,
+                                                       activityIndicator: self.activityIndicator)
+                        self.showAlert(title: "Error",
+                                       message: error.localizedDescription)
                     }
                 }
             }
         } else {
             if let error = error {
-                self.showHideActivityIndicator(show: false, activityIndicator: self.activityIndicator)
-                showAlert(title: "Error login", message: error.localizedDescription)
+                showHideActivityIndicator(show: false,
+                                                activityIndicator: activityIndicator)
+                showAlert(title: "Error login",
+                          message: error.localizedDescription)
             }
         }
     }
